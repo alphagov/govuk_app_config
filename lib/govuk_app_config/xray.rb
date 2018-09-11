56c2babd5d730268873c68f32c1ad7ab856d05ba
Michael Walker <mike@barrucadu.co.uk>
2018-09-11 14:21:49 +0100

    Make the X-Ray sampling target and rate configurable

--- lib/govuk_app_config/xray.rb
+++ lib/govuk_app_config/xray.rb
@@ -5,8 +5,8 @@ Rails.application.config.xray = {
   patch: %I[net_http aws_sdk],
   sampling_rules: {
     default: {
-      "fixed_target": 0,
-      "rate": 0.01,
+      "fixed_target": ENV.fetch("XRAY_SAMPLE_TARGET", 0).to_i,
+      "rate": ENV.fetch("XRAY_SAMPLE_RATE", 0.01).to_f,
     },
   },
 }
