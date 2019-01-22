3b7795efcbb5378b239c1c5dee9fa4b98868fce3
Tijmen Brommet <tijmen@gmail.com>
2019-01-22 17:19:41 +0000

    Use `config.before_send` to send things to statsd
    https://github.com/getsentry/raven-ruby/blob/284e39422068b3f3b1b1223a5b4
    3dd6c756c9397/lib/raven/configuration.rb#L163-L170

--- lib/govuk_app_config/configure.rb
+++ lib/govuk_app_config/configure.rb
@@ -3,14 +3,14 @@ if defined?(Airbrake)
 end
 
 GovukError.configure do |config|
-  # We're misusing the `should_capture` block here to hook into raven until
-  # there's a better way: https://github.com/getsentry/raven-ruby/pull/750
-  config.should_capture = Proc.new { |e|
+  config.before_send = Proc.new { |e|
     GovukStatsd.increment("errors_occurred")
 
     # For backwards compatibility
     GovukStatsd.increment("errbit.errors_occurred")
+  }
 
+  config.should_capture = Proc.new { |e|
     exception_class = e.respond_to?(:original_exception) ? e.original_exception.class : e.class
     if exception_class.ancestors.any? { |c| c.name =~ /^GdsApi::(HTTPIntermittent|TimedOutException)/ }
       GovukStatsd.increment("gds_api_adapters.errors.#{e.class.name.demodulize.underscore}")
