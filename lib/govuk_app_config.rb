1359663fc86a0aae16252b9e711531421b7d2c10
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2019-04-05 11:01:47 +0100

    Add support for content security policy
    This commit moves support for content security policy headers from government-frontend to this gem to allow all frontend apps to use it.

--- lib/govuk_app_config.rb
+++ lib/govuk_app_config.rb
@@ -1,4 +1,5 @@
 require "govuk_app_config/version"
+require "govuk_app_config/govuk_content_security_policy"
 require "govuk_app_config/govuk_statsd"
 require "govuk_app_config/govuk_error"
 require "govuk_app_config/govuk_logging"
