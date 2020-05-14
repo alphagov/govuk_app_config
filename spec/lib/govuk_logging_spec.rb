eb9b74a54894b8317d05b18d1cb6eaebf28fa493
callumknights <callum.knights@digital.cabinet-office.gov.uk>
2020-05-14 11:47:29 +0100

    Add a basic test of the default rails logger
    Co-authored-by: Richard Towers <richard.towers@digital.cabinet-office.gov.uk>

--- spec/lib/govuk_logging_spec.rb
+++ spec/lib/govuk_logging_spec.rb
@@ -5,13 +5,21 @@ require 'govuk_app_config/govuk_logging'
 RSpec.describe GovukLogging do
   class DummyLoggingRailsApp < Rails::Application; end
 
+  old_stderr = nil
+
   let(:fake_stdout) { StringIO.new }
   let(:info_log_level) { 1 }
 
   before do
+    old_stderr = $stderr
+    $stderr = StringIO.new
     allow($stdout).to receive(:clone).and_return(fake_stdout)
     allow($stdout).to receive(:reopen)
     Rails.logger = Logger.new(fake_stdout, level: info_log_level)
+  end
+
+  after do
+    $stderr = old_stderr
   end
 
   describe '.configure' do
@@ -35,6 +43,15 @@ RSpec.describe GovukLogging do
       fake_stdout.rewind
 
       expect(fake_stdout.read).to match(/test log entry/)
+    end
+
+    it 'can write to default rails logger' do
+      GovukLogging.configure
+      logger = Rails.logger
+      logger.info('test default log entry')
+      $stderr.rewind
+
+      expect($stderr.read).to match(/test default log entry/)
     end
   end
 end
