799fe6b7588b43b3f9d6ace39e0c8e523104d942
Michael Walker <mike@barrucadu.co.uk>
2020-07-20 14:39:34 +0100

    Fix Naming/MemoizedInstanceVariableName

--- lib/govuk_app_config/govuk_statsd.rb
+++ lib/govuk_app_config/govuk_statsd.rb
@@ -7,7 +7,7 @@ module GovukStatsd
                  :gauge, :set, :batch
 
   def self.client
-    @statsd_client ||= begin
+    @client ||= begin
       statsd_client = ::Statsd.new("localhost")
       statsd_client.namespace = ENV["GOVUK_STATSD_PREFIX"].to_s
       statsd_client
