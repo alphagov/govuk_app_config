b15a93bca3c4e07f19379680869764f441bad9e6
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2020-10-12 12:58:35 +0100

    Move configuration to configure.rb
    Previous class 'ConfigureDefaults' doesn't really behave like a class as
    all it does is use a function to apply side effects to the argument.
    It is cleaner to move the default configuration here (alternatively it
    could have been inline in govuk_app_config.rb itself but that felt like
    we were burdening the file with too much responsibility.)
    
    It does become very difficult to test the config (and would rely
    heavily upon mocks and stubbing), so I've sacrificed the
    'silence_ready' tests, which were the only ones that were arguably
    useful. Ideally our default config should be as hard-coded and
    predictable as possible, so that tests aren't even necessary.

--- lib/govuk_app_config.rb
+++ lib/govuk_app_config.rb
@@ -1,15 +1,11 @@
 require "govuk_app_config/version"
 require "govuk_app_config/govuk_statsd"
 require "govuk_app_config/govuk_error"
+require "govuk_app_config/govuk_error/configure"
 require "govuk_app_config/govuk_healthcheck"
 # This require is deprecated and should be removed on next major version bump
 # and should be required by applications directly.
 require "govuk_app_config/govuk_unicorn"
-require "govuk_app_config/govuk_error/configure_defaults"
-
-GovukError.configure do |config|
-  GovukError::ConfigureDefaults.new(config)
-end
 
 if defined?(Rails)
   require "govuk_app_config/govuk_logging"
