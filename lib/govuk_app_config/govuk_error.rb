25395c7a1c367e96667eb8e5bbe0cc86bfbc701f
Benjamin Eskola <benjamin.eskola@digital.cabinet-office.gov.uk>
2020-12-03 15:49:18 +0000

    Add govuk_app_config version to every sentry call
    This is to simplify debugging problems with error handling, see:
    https://trello.com/c/guK81BGF/2239-3-send-govukappconfig-version-with-errors

--- lib/govuk_app_config/govuk_error.rb
+++ lib/govuk_app_config/govuk_error.rb
@@ -1,6 +1,7 @@
 require "sentry-raven"
 require "govuk_app_config/govuk_statsd"
 require "govuk_app_config/govuk_error/configuration"
+require "govuk_app_config/version"
 
 module GovukError
   def self.notify(exception_or_message, args = {})
@@ -8,6 +9,9 @@ module GovukError
     # client, allowing easy upgrades.
     args[:extra] ||= {}
     args[:extra].merge!(parameters: args.delete(:parameters))
+
+    args[:tags] ||= {}
+    args[:tags][:govuk_app_config_version] = GovukAppConfig::VERSION
 
     Raven.capture_exception(exception_or_message, args)
   end
