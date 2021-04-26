cda4666d7dea9552a0ff9c6482a2c60d453db874
Michael Walker <mike@barrucadu.co.uk>
2021-04-26 10:59:17 +0100

    Remove unsuitable healthchecks
    These are not suitable as healthchecks because they indicate a problem
    outside of the running app.

--- lib/govuk_app_config/govuk_healthcheck.rb
+++ lib/govuk_app_config/govuk_healthcheck.rb
@@ -4,10 +4,6 @@ require "govuk_app_config/govuk_healthcheck/mongoid"
 require "govuk_app_config/govuk_healthcheck/rails_cache"
 require "govuk_app_config/govuk_healthcheck/redis"
 require "govuk_app_config/govuk_healthcheck/sidekiq_redis"
-require "govuk_app_config/govuk_healthcheck/threshold_check"
-require "govuk_app_config/govuk_healthcheck/sidekiq_queue_check"
-require "govuk_app_config/govuk_healthcheck/sidekiq_queue_latency_check"
-require "govuk_app_config/govuk_healthcheck/sidekiq_retry_size_check"
 require "json"
 
 module GovukHealthcheck
