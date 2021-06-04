ce2b31af120376f10cbd9e5fae2cf864d21c3873
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2021-06-04 14:43:47 +0100

    Remove `silence_ready` configuration option
    We were seeing errors:
    
    ```
      1) GovukError.configure should contain only valid Sentry config
         Failure/Error: config.silence_ready = !Rails.env.production? if defined?(Rails)
    
         NoMethodError:
           undefined method `silence_ready=' for #<GovukError::Configuration:0x000055c592cd9e18>
          ./lib/govuk_app_config/govuk_error/configure.rb:2:in `block in <top (required)>'
          ./lib/govuk_app_config/govuk_error.rb:21:in `configure'
          ./lib/govuk_app_config/govuk_error/configure.rb:1:in `<top (required)>'
          /var/lib/jenkins/bundles/ruby/2.6.0/gems/activesupport-6.1.3.2/lib/active_support/dependencies.rb:332:in `require'
          /var/lib/jenkins/bundles/ruby/2.6.0/gems/activesupport-6.1.3.2/lib/active_support/dependencies.rb:332:in `block in require'
          /var/lib/jenkins/bundles/ruby/2.6.0/gems/activesupport-6.1.3.2/lib/active_support/dependencies.rb:299:in `load_dependency'
          /var/lib/jenkins/bundles/ruby/2.6.0/gems/activesupport-6.1.3.2/lib/active_support/dependencies.rb:332:in `require'
          ./spec/govuk_error/configure_spec.rb:7:in `block (2 levels) in <top (required)>'
          /var/lib/jenkins/bundles/ruby/2.6.0/gems/webmock-3.13.0/lib/webmock/rspec.rb:37:in `block (2 levels) in <top (required)>'
    ```
    
    It doesn't appear to be in the migration guide, and is still
    listed as an option in https://docs.sentry.io/clients/ruby/configuration/options/,
    but the functionality is trivial, so no real harm in removing.

--- lib/govuk_app_config/govuk_error/configure.rb
+++ lib/govuk_app_config/govuk_error/configure.rb
@@ -1,6 +1,4 @@
 GovukError.configure do |config|
-  config.silence_ready = !Rails.env.production? if defined?(Rails)
-
   # These are the environments (described by the `SENTRY_CURRENT_ENV`
   # ENV variable) where we want to capture Sentry errors. If
   # `SENTRY_CURRENT_ENV` isn't in this list, or isn't defined, then
