e729d5d1743b317d2d4d8a9bcfbc430416a3fe09
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2021-06-08 12:51:18 +0100

    WIP: hacking together a fix

--- lib/govuk_app_config/govuk_error.rb
+++ lib/govuk_app_config/govuk_error.rb
@@ -21,4 +21,19 @@ module GovukError
     @configuration ||= Configuration.new(Sentry::Configuration.new)
     yield @configuration
   end
+
+  def self.init
+    Sentry.init do |config|
+      # system configuration properties, required for Sentry to work
+      config.dsn = @configuration.dsn.instance_variable_get(:@raw_value)
+      config.background_worker_threads = @configuration.background_worker_threads
+      config.transport.transport_class = @configuration.transport.transport_class
+
+      # actual configuration - make sure this is in sync with govuk_error/configure.rb
+      config.before_send = @configuration.before_send
+      config.enabled_environments = @configuration.enabled_environments
+      config.excluded_exceptions = @configuration.excluded_exceptions
+      config.inspect_exception_causes_for_exclusion = @configuration.inspect_exception_causes_for_exclusion
+    end
+  end
 end
--- spec/lib/govuk_error_spec.rb
+++ spec/lib/govuk_error_spec.rb
@@ -34,4 +34,64 @@ RSpec.describe GovukError do
         .to yield_with_args(instance_of(GovukError::Configuration))
     end
   end
+
+  describe ".init" do
+    let(:dummy_dsn) { "http://12345:67890@sentry.localdomain/sentry/42" }
+
+    it "calls Sentry.init" do
+      expect(Sentry).to receive(:init).once
+      GovukError.init
+    end
+
+    it "configures Sentry with all previous GovukError.configure calls" do
+      stub_const("ErrorWeWantToIgnore", Class.new(StandardError))
+      stub_const("DataSyncIgnorableError", Class.new(StandardError))
+      stub_request(:post, "http://sentry.localdomain/sentry/api/42/envelope/").to_return(status: 200)
+
+      ClimateControl.modify SENTRY_CURRENT_ENV: "integration-blue-aws", GOVUK_DATA_SYNC_PERIOD: "22:00-08:00" do
+        travel_to(Time.current.change(hour: 23)) do
+          GovukError.configure do |config|
+            config.excluded_exceptions = %w[Foo]
+          end
+          GovukError.configure do |config|
+            config.excluded_exceptions += %w[Bar]
+            config.enabled_environments = %w[integration-blue-aws]
+            config.data_sync_excluded_exceptions = %w[DataSyncIgnorableError]
+            config.before_send = lambda do |event, hint|
+              hint[:exception].is_a?(ErrorWeWantToIgnore) ? nil : event
+            end
+
+            # some hackery to get the test to work
+            config.dsn = dummy_dsn
+            config.transport.transport_class = Sentry::DummyTransport
+            # so the events will be sent synchronously for testing
+            config.background_worker_threads = 0
+          end
+          GovukError.init
+
+          client = Sentry.get_current_client
+          expect(client.configuration.sending_allowed?).to eq(true)
+          expect(client.configuration.excluded_exceptions).to eq(%w[Foo Bar])
+
+          # NOTE: we can't call this getter method as Sentry::Configuration hasn't defined it
+          # expect(config.data_sync_excluded_exceptions).to eq(%w[Baz])
+
+          expect { Sentry.capture_exception(ErrorWeWantToIgnore.new("Dummy exception")) }
+            .to change { client.transport.events.count }
+            .by(0)
+
+          # @TODO - this assertion fails with this seed:
+          #     bundle exec rspec --seed 12454
+          # ...but passes with this seed:
+          #     bundle exec rspec --seed 30962
+          expect { Sentry.capture_exception(DataSyncIgnorableError.new("Exception we want to ignore during data sync")) }
+            .to change { client.transport.events.count }
+            .by(0)
+          expect { Sentry.capture_exception(StandardError.new("Real exception")) }
+            .to change { client.transport.events.count }
+            .by(1)
+        end
+      end
+    end
+  end
 end
