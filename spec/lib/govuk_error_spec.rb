a3967c7a28f609e7747ada95dae9f79380eb7198
Kevin Dew <kevindew@me.com>
2022-04-04 20:44:00 +0100

    Call original on Sentry methods to reveal issues
    This configures the mocks for this method to call original so that in
    any problems with the API can be exposed - as far as I can tell calling
    these has no side effects.
    
    The reason I'm adding this in is because these methods cause errors on
    Ruby 3, but because we use mocks our test don't expose it. By calling
    original these tests raise errors.

--- spec/lib/govuk_error_spec.rb
+++ spec/lib/govuk_error_spec.rb
@@ -3,35 +3,29 @@ require "govuk_app_config/govuk_error"
 
 RSpec.describe GovukError do
   describe ".notify" do
+    before do
+      # Call original on these to ensure we discover any problems with the method interfaces
+      allow(Sentry).to receive(:capture_exception).and_call_original
+      allow(Sentry).to receive(:capture_message).and_call_original
+    end
+
     it "forwards the exception" do
-      allow(Sentry).to receive(:capture_exception)
-
       GovukError.notify(StandardError.new)
-
       expect(Sentry).to have_received(:capture_exception)
     end
 
     it "forwards the string" do
-      allow(Sentry).to receive(:capture_message)
-
       GovukError.notify("Foo")
-
       expect(Sentry).to have_received(:capture_message)
     end
 
     it "allows Airbrake-style parameters" do
-      allow(Sentry).to receive(:capture_exception)
-
       GovukError.notify(StandardError.new, parameters: "Something")
-
       expect(Sentry).to have_received(:capture_exception).with(StandardError.new, hash_including(extra: { parameters: "Something" }))
     end
 
     it "sends the version along with the request" do
-      allow(Sentry).to receive(:capture_exception)
-
       GovukError.notify(StandardError.new, parameters: "Something")
-
       expect(Sentry).to have_received(:capture_exception).with(StandardError.new, hash_including(tags: { govuk_app_config_version: /^[0-9]\.[0-9]\.[0-9](?:\.pre\.[0-9]+)?$/ }))
     end
   end
