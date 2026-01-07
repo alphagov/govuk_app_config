c62ae1d3b850dd0471957de4d1e30e6c0090ff11
Sonia Hussain <56222256+CodeSonia@users.noreply.github.com>
2026-01-07 12:33:49 +0000

    Remove StaticProxy middleware and tests

--- lib/govuk_app_config/railtie.rb
+++ lib/govuk_app_config/railtie.rb
@@ -2,12 +2,6 @@ require "plek"
 
 module GovukAppConfig
   class Railtie < Rails::Railtie
-    initializer "govuk_app_config.configure_govuk_proxy" do |app|
-      if ENV["GOVUK_PROXY_STATIC_ENABLED"] == "true"
-        app.middleware.use GovukProxy::StaticProxy, backend: Plek.find("static")
-      end
-    end
-
     initializer "govuk_app_config.configure_open_telemetry" do |app|
       unless Rails.const_defined?(:Console)
         GovukOpenTelemetry.configure(app.class.module_parent_name.underscore)
